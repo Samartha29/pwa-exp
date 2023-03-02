5416b53314cc3ea6003e214f41f13ee96cb0543d
omkarchugh2020 <110529882+omkarchugh2020@users.noreply.github.com>
2023-03-02 19:08:50 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,68 @@
-if ('Notification' in window) {
-  Notification.requestPermission().then(function(permission) {
-    if (permission === 'granted') {
-      // show notification
-    }
-  });
+self.addEventListener("install", function (event) {
+event.waitUntil(preLoad());
+});
+self.addEventListener("fetch", function (event) {
+event.respondWith(
+checkResponse(event.request).catch(function () {
+console.log("Fetch from cache successful!");
+return returnFromCache(event.request);
+})
+);
+console.log("Fetch successful!");
+event.waitUntil(addToCache(event.request));
+});
+self.addEventListener("sync", (event) => {
+if (event.tag === "syncMessage") {
+console.log("Sync successful!");
 }
-
-self.addEventListener('fetch', event => {
-  console.log('Fetch event triggered:', event.request.url);
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          console.log('Cache hit:', event.request.url);
-          return response;
-        }
-
-        console.log('Cache miss:', event.request.url);
-        return fetch(event.request);
-      })
-      .catch(error => {
-        console.log('Fetch error:', error);
-      })
-  );
 });
-
-self.addEventListener('push', event => {
-  console.log('Push event triggered:', event.data.text());
-
-  const title = 'Push Notification';
-  const options = {
-    body: event.data.text(),
-    icon: 'images/icon.png'
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
+self.addEventListener("push", function (event) {
+if (event && event.data) {
+var data = event.data.json();
+if (data.method == "pushMessage") {
+console.log("Push notification sent");
+event.waitUntil(
+self.registration.showNotification("Portfolio Pro", {
+body: data.message,
+icon: "/windows11/LargeTile.scale-100.png",
+})
+);
+}
+}
 });
-
-self.addEventListener('sync', event => {
-  console.log('Sync event triggered:', event.tag);
-
-  if (event.tag === 'sync-example') {
-    event.waitUntil(
-      // Perform sync operation here
-    );
-  }
+var filesToCache = ["/index.html"];
+var preLoad = function () {
+  return caches.open("offline").then(function (cache) {
+// caching index and important routes
+return cache.addAll(filesToCache);
 });
+};
+var checkResponse = function (request) {
+return new Promise(function (fulfill, reject) {
+fetch(request).then(function (response) {
+if (response.status !== 404) {
+fulfill(response);
+} else {
+reject();
+}
+}, reject);
+});
+};
+var addToCache = function (request) {
+return caches.open("offline").then(function (cache) {
+return fetch(request).then(function (response) {
+return cache.put(request, response);
+});
+});
+};
+var returnFromCache = function (request) {
+return caches.open("offline").then(function (cache) {
+return cache.match(request).then(function (matching) {
+if (!matching || matching.status == 404) {
+return cache.match("offline.html");
+} else {
+return matching;
+}
+});
+});
+};
